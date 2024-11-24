983eb0d8fe8cd1901e5fe35635b6d332c23d853e
tomerkassorla01 <tomer.kassorla@post.runi.ac.il>
2024-11-24 20:24:34 +0200

    done algebra 2

--- Algebra.java
+++ Algebra.java
@@ -25,16 +25,30 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1++;
+		if (x2 > 0) {
+			for (int i = 0; i < x2; i++) {
+				x1++;
+			}
+		}
+		else {
+			for (int i = x2; i < 0; i++) {
+				x1--;
+			}
 		}
 		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		for (int i = 0; i < x2; i++) {
-			x1--;
+		if (x2 > 0) {
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
+		}
+		else {
+			for (int i = x2; i < 0; i++) {
+				x1++;
+			}
 		}
 		return x1;
 	}
@@ -42,6 +56,12 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int sum = 0;
+		if(x1 < 0) {
+			minus(0, x1);
+		}
+		if(x2 < 0) {
+			minus(0, x2);
+		}
 		for (int i = 0; i < x2; i++) {
 			sum = plus(sum, x1);
 		}
